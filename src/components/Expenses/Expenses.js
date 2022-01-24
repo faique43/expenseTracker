9aa904d9a73e60e3add75ed88dcadc0d54296455
Faique Ahmad Khan <fkhan.bese20seecs@seecs.edu.pk>
2022-01-24 18:59:18 +0500

    new

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+import ExpenseFilter from "./ExpenseFilter";
+import Card from "../UI/Card";
+import "./Expenses.css";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2022");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <li>
+      <Card className="expenses">
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </li>
+  );
+}
+
+export default Expenses;
